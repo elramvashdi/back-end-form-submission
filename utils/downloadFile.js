876344fbcc77b9622a4c0ab5331deae701079f4b
Elram Vashdi <elramv@Elram-Vashdi-YPKJGFYM0Y.local>
2023-06-11 12:17:51 +0300

    pdf-sting-template

--- utils/downloadFile.js
+++ utils/downloadFile.js
@@ -1,10 +1,10 @@
 export const downloadFile =  (url,fileName) =>{
   return `
-    fetch(url).then(data => {
+    fetch('${url}').then(data => {
       data.blob().then((blobFile)=> {
         var a = document.createElement("a");
         a.href = URL.createObjectURL(blobFile);
-        a.setAttribute("download", filename);
+        a.setAttribute("download", '${fileName}');
         a.click();
         a.remove();
       }

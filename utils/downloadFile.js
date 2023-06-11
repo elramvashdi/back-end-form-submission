8b13ae98fd60e308f18ecd4227822015e2b3a49c
Elram Vashdi <elramv@Elram-Vashdi-YPKJGFYM0Y.local>
2023-06-11 11:20:36 +0300

    pdf-remove-script

--- utils/downloadFile.js
+++ utils/downloadFile.js
@@ -1,12 +1,10 @@
 export const downloadFile =  (url,fileName) =>{
   return `
-  <script>
     const aTag = document.createElement('a');
     aTag.href = ${url}
     aTag.setAttribute('download', ${fileName})
     document.body.appendChild(aTag);
     aTag.click();
-    aTag.remove(); 
-  </script>
-  `
+    aTag.remove();
+    `
     }

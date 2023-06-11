0c8cca9decd2ae68b7b160b27acc83e256c021a2
Elram Vashdi <elramv@Elram-Vashdi-YPKJGFYM0Y.local>
2023-06-11 11:34:15 +0300

    pdf-wrap-strings

--- utils/downloadFile.js
+++ utils/downloadFile.js
@@ -1,8 +1,8 @@
 export const downloadFile =  (url,fileName) =>{
   return `
     const aTag = document.createElement('a');
-    aTag.href = ${url}
-    aTag.setAttribute('download', ${fileName})
+    aTag.href = ' ${url} '
+    aTag.setAttribute('download', ' ${fileName} ')
     document.body.appendChild(aTag);
     aTag.click();
     aTag.remove();
